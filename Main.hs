229795f59443ac5e2711bb4a4dff48ff37101e0f
Vit0rSilveira <vitor.silveira98@gmail.com>
2023-05-29 20:40:56 -0300

    Arrumando a função, resultado está correto, mas falta alterar umas coisas

--- Main.hs
+++ Main.hs
@@ -1,38 +1,23 @@
--- Função auxiliar para calcular a pontuação de um frame
-pontuacaoFrame :: [Int] -> Int -> Int --Declarando a função
-pontuacaoFrame jogadas frame -- Definindo a função a partir daqui
-  | ehStrike jogadas frame = 10 + bonusStrike jogadas frame
-  | ehSpare jogadas frame = 10 + bonusSpare jogadas frame
-  | otherwise = soma jogadas frame
+pontuacaoTotalAux :: [Int] -> Int -> Int -> Int
+pontuacaoTotalAux [] _ pontuacao = pontuacao
+pontuacaoTotalAux (x:y:z:resto) rodada pontuacao
+    | rodada > 10 = pontuacao
+    | ehStrike x = pontuacaoTotalAux (y:z:resto) (rodada + 1) (pontuacao + x + y + z)
+    | ehSpare x y = pontuacaoTotalAux (z:resto) (rodada + 1) (pontuacao + x + y + z)
+    | otherwise = pontuacaoTotalAux (z:resto) (rodada + 1) (pontuacao + x + y)
+pontuacaoTotalAux _ _ pontuacao = pontuacao
 
 -- Verifica se o frame é um strike
-ehStrike :: [Int] -> Int -> Bool
-ehStrike jogadas frame = jogadas !! frame == 10
+ehStrike :: Int -> Bool
+ehStrike x = x == 10
 
 -- Verifica se o frame é um spare
-ehSpare :: [Int] -> Int -> Bool
-ehSpare jogadas frame = soma jogadas frame == 10
-
--- Calcula a soma das duas jogadas de um frame
-soma :: [Int] -> Int -> Int
-soma jogadas frame = jogadas !! frame + jogadas !! (frame + 1)
-
--- Calcula o bônus de um strike
-bonusStrike :: [Int] -> Int -> Int
-bonusStrike jogadas frame
-  | frame == 9 = jogadas !! 10 + jogadas !! 11
-  | ehStrike jogadas frame = 10 + jogadas !! (frame + 1) + bonusStrike jogadas (frame + 1)
-  | otherwise = soma jogadas (frame + 1)
-
--- Calcula o bônus de um spare
-bonusSpare :: [Int] -> Int -> Int
-bonusSpare jogadas frame
-  | frame == 9 = jogadas !! 10 + jogadas !! 11
-  | otherwise = jogadas !! (frame + 2)
+ehSpare :: Int -> Int -> Bool
+ehSpare x y = x + y == 10
 
 -- Calcula a pontuação final
-pontuacaoFinal :: [Int] -> Int --Recebe um [Int] (input da função) e retorna um Int (output da função)
-pontuacaoFinal jogadas = sum [pontuacaoFrame jogadas frame | frame <- [0 .. 9]]
+pontuacaoFinal :: [Int] -> Int
+pontuacaoFinal jogadas = pontuacaoTotalAux jogadas 1 0
 
 -- Função principal para ler a sequência de pinos e imprimir a pontuação final
 main :: IO ()
@@ -40,10 +25,8 @@ main = do
   putStrLn "Digite a sequência de pinos derrubados (use espaços para separar):"
   entrada <- getLine
   let pinos = map read (words entrada) :: [Int]
-  let pontuacao = pontuacaoFinal pinos
+  let pontos = pontuacaoFinal pinos
   putStrLn ("Sequência de pinos derrubados: " ++ show pinos)
-  putStrLn ("Pontuação final: " ++ show pontuacao)
+  putStrLn $ "Pontuação final do jogo: " ++ show pontos
 
-
--- sum: soma todos os elementos de uma lista
 -- show: imprime itens como um String
